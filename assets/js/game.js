a40013a5086f5eaad14313c37ad6acfd93ea6998
thevcr <thevcr@gmail.com>
2022-03-02 20:44:55 -0500

    update enemy combatant loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,9 +8,7 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
-  
+   while(enemyHealth > 0) {
     // ask player if they'd like to fight or run
     var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
   
@@ -60,9 +58,12 @@ var fight = function(enemyName) {
     } else {
       window.alert("You need to pick a valid option. Try again!");
     }
+   }
   };
   
   // run fight function to start game
   for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
   }  
